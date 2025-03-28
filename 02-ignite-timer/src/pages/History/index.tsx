5c80c706ad5013f0f09eab2c49a339efde4769ad
Mosiah Andrade <mosiahassuncao@GMAIL.COM>
2025-03-28 14:47:37 +0000

    update form validation

--- 02-ignite-timer/src/pages/History/index.tsx
+++ 02-ignite-timer/src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
     return(
@@ -19,49 +19,49 @@ export function History() {
                                 <td>Tarefa</td>
                                 <td>20 minutos</td>
                                 <td>Há 2 mêses</td>
-                                <td>Concluído</td>
+                                <Status statusColor="green">Concluido</Status>
                             </tr>
                             <tr>
                                 <td>Tarefa</td>
                                 <td>20 minutos</td>
                                 <td>Há 2 mêses</td>
-                                <td>Concluído</td>
+                                <Status statusColor="green">Concluido</Status>
                             </tr>
                             <tr>
                                 <td>Tarefa</td>
                                 <td>20 minutos</td>
                                 <td>Há 2 mêses</td>
-                                <td>Concluído</td>
+                                <Status statusColor="green">Concluido</Status>
                             </tr>
                             <tr>
                                 <td>Tarefa</td>
                                 <td>20 minutos</td>
                                 <td>Há 2 mêses</td>
-                                <td>Concluído</td>
+                                <Status statusColor="green">Concluido</Status>
                             </tr>
                             <tr>
                                 <td>Tarefa</td>
                                 <td>20 minutos</td>
                                 <td>Há 2 mêses</td>
-                                <td>Concluído</td>
+                                <Status statusColor="green">Concluido</Status>
                             </tr>
                             <tr>
                                 <td>Tarefa</td>
                                 <td>20 minutos</td>
                                 <td>Há 2 mêses</td>
-                                <td>Concluído</td>
+                                <Status statusColor="green">Concluido</Status>
                             </tr>
                             <tr>
                                 <td>Tarefa</td>
                                 <td>20 minutos</td>
                                 <td>Há 2 mêses</td>
-                                <td>Concluído</td>
+                                <Status statusColor="green">Concluido</Status>
                             </tr>
                             <tr>
                                 <td>Tarefa</td>
                                 <td>20 minutos</td>
                                 <td>Há 2 mêses</td>
-                                <td>Concluído</td>
+                                <Status statusColor="green">Concluido</Status>
                             </tr>
                         </tbody>
                     </table>
--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -1,17 +1,47 @@
 import { Play } from "phosphor-react";
-import { CountContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from "./styles";
+import {zodResolver} from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+import { CountContainer,
+     FormContainer, 
+     HomeContainer, 
+     MinutesAmountInput, 
+     Separator, 
+     StartCountDownButton, 
+     TaskInput } from "./styles";
+
+import {useForm} from 'react-hook-form'
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    MinutesAmount: zod.number().min(5, 'O ciclo precisa ser de no min 5 minutos').max(60, 'O ciclo precisa ser no maximo de 60 minutos')
+})
 
 export function Home() {
+    
+    const {register, handleSubmit, watch} = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema)
+    })
+
+    function handleCreateNewCycle(data) {
+        console.log(data)
+    }
+
+    const task = watch('task')
+    const isSubmitDisabled = !task;
+
+
     return(
         <HomeContainer>
-            <form action="">
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput 
-                    type="text" 
-                    id="task"  
-                    list="task-suggestions"
-                    placeholder="Dê um nome para o seu projeto"/>
+                        type="text" 
+                        id="task"  
+                        list="task-suggestions"
+                        placeholder="Dê um nome para o seu projeto"
+                        {...register('task')}
+                    />
 
                     <datalist id="task-suggestions">
                         <option value="Projeto 1"/>
@@ -26,7 +56,9 @@ export function Home() {
                     placeholder="00" 
                     step={5}
                     min={5}
-                    max={60}/>
+                    max={60}
+                    {...register('minuteAmount',{ valueAsNumber: true })}
+                />
 
                     <span>minutos.</span>
                 </FormContainer>
@@ -39,7 +71,7 @@ export function Home() {
                     <span>0</span>
                 </CountContainer>
 
-                <StartCountDownButton  type="submit" disabled>
+                <StartCountDownButton  type="submit" disabled={isSubmitDisabled}>
                     <Play />
                     Começar
                 </StartCountDownButton>
