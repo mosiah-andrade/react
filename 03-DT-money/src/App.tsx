83010b2d6e82f3ca2859daf0b92b114938a2ae02
Mosiah Andrade <mosiahassuncao@GMAIL.COM>
2025-04-06 20:19:59 +0000

    DT-MONEY -setup

---
+++ 03-DT-money/src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/theme/default"
+import { GlobalStyle } from "./styles/global"
+
+
+export function App() {
+ 
+  return (
+    <ThemeProvider theme={defaultTheme}>
+
+      <GlobalStyle />
+      
+      <h1>Hello world</h1>
+
+    </ThemeProvider>
+  )
+}
+
