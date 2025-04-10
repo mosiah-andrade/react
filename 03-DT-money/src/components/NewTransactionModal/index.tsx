cfcca4e189e69fa8be96068cf6cf1d95519fa9c1
Mosiah Andrade <mosiahassuncao@GMAIL.COM>
2025-04-10 19:07:46 +0000

    Botões de entrada e saída

--- 03-DT-money/src/components/NewTransactionModal/index.tsx
+++ 03-DT-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Diolog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay } from './styles'
-import { X } from 'phosphor-react'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 
 
 export function NewTransactionModal() {
@@ -20,6 +20,18 @@ export function NewTransactionModal() {
                         <input type="text" placeholder='Descrição' required />
                         <input type="number" placeholder='Preço' required />
                         <input type="text" placeholder='Categoria' required />
+
+                        <TransactionType>
+                            <TransactionTypeButton variante='income'>
+                                <ArrowCircleUp size={24} />
+                                Entrada
+                            </TransactionTypeButton>
+                            <TransactionTypeButton variante='outcome'>
+                                <ArrowCircleDown size={24} />
+                                Saída 
+                            </TransactionTypeButton>
+
+                        </TransactionType>
 
                         <button type="submit">
                             Cadastrar
