6cd582a77cb79d1078d3deb4fdf2f48b4a4c0453
mosiah-andrade <mosiahandrade@gmail.com>
2025-04-14 14:21:25 -0300

    fix issues

--- 03-DT-money/src/pages/Transactions/index.tsx
+++ 03-DT-money/src/pages/Transactions/index.tsx
@@ -1,58 +1,59 @@
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { SearchForm } from "./components/searchForm";
-import { PriceHightlight, TransactionContainer, TransactionTable } from "./styles";
-import { useState, useEffect } from "react";
-
+import { Header } from "../../components/Header"
+import { Summary } from "../../components/Summary"
+import { SearchForm } from "./components/searchForm"
+import { PriceHightlight, TransactionContainer, TransactionTable } from "./styles"
+import { useState, useEffect } from "react"
 
 interface Transaction {
-    id: number;
-    description: string;
-    type: 'income'| 'outcome';
-    price: number;
-    category: string;
-    createdAt: string;
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
 }
 
-
 export function Transactions() {
-    const [transactions, setTransactions] = useState<Transaction>([])
+  const [transactions, setTransactions] = useState<Transaction[]>([])
 
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions')
+    const data = await response.json()
+    setTransactions(data)
+    console.log(data)
+  }
 
-    async function loadTransactions() {
-        const response = await fetch('https://expert-train-v6rjj7gq45xpcx996-4000.app.github.dev/')
-        const data = await response.json()
-        setTransactions(data)
-        console.log(data)
-    }
+  useEffect(() => {
+    loadTransactions()
+  }, [])
 
-    useEffect(() => {
-        loadTransactions()
-    }, [])
+  return (
+    <div>
+      <Header />
+      <Summary />
 
-    return (
-        <div>
-            <Header />
-            <Summary/>
-
-           <TransactionContainer>
-                <SearchForm/>
-                <TransactionTable>
-                    <tbody>
-                        {transactions.map(transaction => {
-                            return (
-                                <tr key={transaction.id}>
-                                    <td width="50%">{transaction.description}</td>
-                                        <PriceHightlight variant={transaction.type}>R$ {transaction.price}</PriceHightlight>
-                                    <td>{transaction.category}</td>
-                                    <td>{transaction.createAt}</td>
-                                </tr>
-                            )
-                        })}
-                    
-                    </tbody>
-                </TransactionTable>
-           </TransactionContainer>
-        </div>
-    )
+      <TransactionContainer>
+        <SearchForm />
+        <TransactionTable>
+          <tbody>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <PriceHightlight variant={transaction.type}>
+                    {new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL'
+                    }).format(transaction.price)}
+                  </PriceHightlight>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
+    </div>
+  )
 }
